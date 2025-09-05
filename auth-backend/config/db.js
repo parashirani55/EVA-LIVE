b81ec509621c3f7b716c0bb9f14644f6ac515700
parashirani55 <78304896+parashirani55@users.noreply.github.com>
2025-09-05 10:50:56 +0530

    Update db.js

--- auth-backend/config/db.js
+++ auth-backend/config/db.js
@@ -10,6 +10,9 @@ const db = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306
+  waitForConnections: true,
+  connectionLimit: 50,   // safe limit for pool
+  queueLimit: 0
 });
 
 db.connect((err) => {
